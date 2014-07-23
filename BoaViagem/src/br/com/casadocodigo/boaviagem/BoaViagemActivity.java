9f0dd7d971e4f51fdbba605820b5b13bc4bdb5bc
Antonio Martins Miranda <amartmiranda@gmail.com>
2014-07-23 17:02:48 -0300

    Implementação simples da funcionalidade que permite ao usuário manter-se logado no programa.

--- BoaViagem/src/br/com/casadocodigo/boaviagem/BoaViagemActivity.java
+++ BoaViagem/src/br/com/casadocodigo/boaviagem/BoaViagemActivity.java
@@ -2,15 +2,20 @@ package br.com.casadocodigo.boaviagem;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class BoaViagemActivity extends Activity {
 	
+	private static final String MANTER_CONECTADO = "manter_conectado";
 	private EditText usuario;
 	private EditText senha;
+	private CheckBox manterConectado;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,14 @@ public class BoaViagemActivity extends Activity {
 		setContentView(R.layout.login);
 		usuario = (EditText) findViewById(R.id.usuario);
 		senha = (EditText) findViewById(R.id.senha);
+		
+		// Permite o login automático, caso o checkbox manter_conectado estiver marcado 
+		manterConectado = (CheckBox) findViewById(R.id.manterConectado);
+		SharedPreferences preferencias = getPreferences(MODE_PRIVATE);
+		boolean conectado = preferencias.getBoolean(MANTER_CONECTADO, false);
+		if(conectado){
+			startActivity(new Intent(this,DashboardActivity.class));
+		}
 	}
 	
 	public void entrarOnClick(View v) {
@@ -26,6 +39,12 @@ public class BoaViagemActivity extends Activity {
 		String mensagemErro;
 		Toast toast;
 		if(usuarioInformado.equals("leitor") && senhaInformada.equals("123")) {
+			// Guardando as preferências do usuário
+			SharedPreferences preferencias = getPreferences(MODE_PRIVATE);
+			Editor editor = preferencias.edit();
+			editor.putBoolean(MANTER_CONECTADO, manterConectado.isChecked());
+			editor.commit();
+			
 			startActivity(new Intent(this,DashboardActivity.class));
 		} else {
 			mensagemErro = getString(R.string.erro_autenticacao);
